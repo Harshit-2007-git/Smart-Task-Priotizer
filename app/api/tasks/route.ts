fe15ef87897d765cb7568023562e7cc671b64349
Harshit-2007-git <harshittiwari2226@gmail.com>
2026-02-21 02:10:18 +0530

    Update route.ts

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -5,7 +5,6 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
 )
-
 export async function GET() {
   const { data, error } = await supabase
     .from("tasks")
